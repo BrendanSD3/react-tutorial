202f7aa12c903cc209b4489f5cabefe8f7b59a24
bwall <brendan.wall03@gmail.com>
2020-06-10 10:19:57 +0100

    Updates

---
+++ precache-manifest.50ed295cf2db8511e6710dc5cfa49076.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452636185d737a18d7663a64a015fc8e",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "f36cef527ac5a9565ab5",
+    "url": "/react-tutorial/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "9e490b6f920b4bad2b56",
+    "url": "/react-tutorial/static/css/main.e721cf54.chunk.css"
+  },
+  {
+    "revision": "f36cef527ac5a9565ab5",
+    "url": "/react-tutorial/static/js/2.b8283ef7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.b8283ef7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e490b6f920b4bad2b56",
+    "url": "/react-tutorial/static/js/main.5fae6e3a.chunk.js"
+  },
+  {
+    "revision": "143094d949d4d9b161c1",
+    "url": "/react-tutorial/static/js/runtime-main.174060d8.js"
+  }
+]);
