cb61608990c878a91ab18fbf2a69b5e6d763824b
BrendanSD3 <brendan.wall03@gmail.com>
2020-11-29 22:25:03 +0000

    Added Some new Stuff to my react practice app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-import React, { useState, Fragment } from 'react'
+import React, {useState, Fragment } from 'react'
 import AddUserForm from './forms/AddUserForm'
 import EditUserForm from './forms/EditUserForm'
 import UserTable from './Tables/UserTable'
+import SongsTable from './Tables/SongsTable'
+import AddSongForm from './forms/AddSongForm'
+import EditSongForm from './forms/EditSongForm'
+
 
 const App = () => {
 	// Data
@@ -10,15 +14,22 @@ const App = () => {
 		{ id: 2, name: 'Craig', username: 'siliconeidolon' },
 		{ id: 3, name: 'Ben', username: 'benisphere' },
 	]
-
+	const songData=[
+		{id:1, name:'HighwaytoHell'}
+	]
+	
 	const initialFormState = { id: null, name: '', username: '' }
-
+	const initialSFormState={id:null,name:''}
 	// Setting state
+	const [songs, setSong]=useState(songData)
+	const [ currentSong, setCurrentSong ] = useState(initialSFormState)
+	const [ Songediting, setSEditing ] = useState(false)
+
 	const [ users, setUsers ] = useState(usersData)
 	const [ currentUser, setCurrentUser ] = useState(initialFormState)
 	const [ editing, setEditing ] = useState(false)
 
-	// CRUD operations
+	// CRUD operations USERS
 	const addUser = user => {
 		user.id = users.length + 1
 		setUsers([ ...users, user ])
@@ -41,13 +52,29 @@ const App = () => {
 
 		setCurrentUser({ id: user.id, name: user.name, username: user.username })
 	}
+	// CRUD Operations Songs
+	const updateSong = (id, updatedSong) => {
+		setSEditing(false)
 
+		setSong(songs.map(song => (song.id === id ? updatedSong : song)))
+	}
+
+	const editSong = song => {
+		setSEditing(true)
+
+		setCurrentSong({ id: song.id, name: song.name })
+	}
+	const addSong = song => {
+		song.id = songs.length + 1
+		setSong([ ...songs, song ])
+	}
 	return (
 		<div className="container">
-			<h1>Hello World</h1>
+			
 			<h1>CRUD App with Hooks</h1>
 			<div className="flex-row">
 				<div className="flex-large">
+					
 					{editing ? (
 						<Fragment>
 							<h2>Edit user</h2>
@@ -64,10 +91,31 @@ const App = () => {
 							<AddUserForm addUser={addUser} />
 						</Fragment>
 					)}
+						{Songediting ? (
+						<Fragment>
+							<h2>Edit songs</h2>
+							<EditSongForm
+								Songediting={Songediting}
+								setSEditing={setSEditing}
+								currentSong={currentSong}
+								updateSong={updateSong}
+							/>
+						</Fragment>
+					) : (
+						<Fragment>
+							<h2>Add songs</h2>
+							<AddSongForm addSong={addSong} />
+						</Fragment>
+					)}
 				</div>
 				<div className="flex-large">
 					<h2>View users</h2>
 					<UserTable users={users} editRow={editRow} deleteUser={deleteUser} />
+				</div>
+
+				<div className="flex-large">
+					<h2>View songs</h2>
+					<SongsTable songs={songs} editSong={editSong} />
 				</div>
 			</div>
 		</div>
